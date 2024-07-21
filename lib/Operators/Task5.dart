2e2eb672467c729556f0f8212981db4f4b812809
mahometawad <mahometawad@gmail.com>
2024-07-21 09:52:15 +0000

    edit

--- lib/Operators/Task5.dart
+++ lib/Operators/Task5.dart
@@ -1,18 +1,18 @@
 import 'dart:io';
 
 void main() {
-  print('Enter First Number  :');
+  print('>>Enter First Number  :');
   var number1 = stdin.readLineSync();
   var n1 = num.parse(number1!);
 
-  print('Enter Second Number :');
+  print('>>Enter Second Number :');
   var number2 = stdin.readLineSync();
   var n2 = num.parse(number2!);
 
   if (n2 != 0) {
     var result = n1 / n2;
-    print('Result Of Division is :$result');
+    print('# Result Of Division is :$result');
   } else {
-    print('=>[You can\'t Divide on Zero}]<=');
+    print('=>[You can\'t Divide on Zero]<=');
   }
 }
